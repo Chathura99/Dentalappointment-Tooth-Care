6a56ba2a133232ad9929b452a19aa36fa14d235e
Chathura <chathura.madura11@gmail.com>
2023-12-11 01:47:09 +0530

    datalist

---
+++ src/dentalappointment/data/DataList.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dentalappointment.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class DataList {
+    private DataList(){}
+    public static List<Appointments> appoinmentList = new ArrayList();
+    public static List<Treatment> treatmentList = new ArrayList();
+}
